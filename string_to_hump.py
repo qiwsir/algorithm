e1d783c274ab49660a97cf59ceee243574b8ff4d
qiwsir <qiwsir@gmail.com>
2014-05-26 23:05:51 +0800

    modify one mistake

--- string_to_hump.py
+++ string_to_hump.py
@@ -4,12 +4,18 @@
 def convert(one_string,space_character):    #one_string:输入的字符串；space_character:字符串的间隔符，以其做为分隔标志
 
     string_list = str(one_string).split(space_character)    #将字符串转化为list
+    first = string_list[0].lower()
+    others = string_list[1:] 
 
-    string_hump_list = [word.capitalize() for word in string_list]      #str.capitalize():将字符串的首字母转化为大写
+    others_capital = [word.capitalize() for word in others]      #str.capitalize():将字符串的首字母转化为大写
 
-    hump_string = ''.join(string_hump_list)     #将list组合成为字符串，中间无连接符。驼峰样式。
+    others_capital[0:0] = [first]
+
+    hump_string = ''.join(others_capital)     #将list组合成为字符串，中间无连接符。
 
     return hump_string
 
 if __name__=='__main__':
+    print "the string is:ab-cd-ef"
+    print "convert to hump:"
     print convert("ab-cd-ef","-")
