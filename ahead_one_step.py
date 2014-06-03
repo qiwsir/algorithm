d0f45172b6364aa3cbed59234f4460f515242ff9
qiwsir <qiwsir@gmail.com>
2014-06-03 13:58:42 +0800

    a simple question.

---
+++ ahead_one_step.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+"""
+问题：
+
+定义一个int型的一维数组，包含10个元素，分别赋值为1~10， 然后将数组中的元素都向前移一个位置，
+
+即，a[0]=a[1],a[1]=a[2],…最后一个元素的值是原来第一个元素的值，然后输出这个数组。
+"""
+
+def ahead_one():
+    a = []
+    for i in range(10):
+        a.append(i)
+
+    b = a.pop(0)
+    a.append(b)
+    return a
+
+if __name__ =="__main__":
+    print ahead_one()
