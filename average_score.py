16ce05d2366efd2f40259d0ff5333afb0eca5f38
qiwsir <qiwsir@gmail.com>
2014-06-03 15:38:50 +0800

    print average score

--- average_score.py
+++ average_score.py
@@ -20,9 +20,11 @@ def less_average(score):
     sum_score = sum(score)
     ave_num = sum_score/num
     less_ave = [i for i in score if i<ave_num]
-    return len(less_ave)
+    return (ave_num,len(less_ave))
 
 if __name__=="__main__":
     score = make_score(40)
-    print "the number of less average is:",less_average(score)
+    average_num,less_num = less_average(score)
+    print 'the score of average is:',average_num
+    print "the number of less average is:",less_num
     print "the every socre is[from big to small]:",sorted(score,reverse=True)
