73fd32ba806f4dae9618bef7e0fd5a730a02d64b
qiwsir <qiwsir@gmail.com>
2014-06-07 10:03:01 +0800

    判断素数的几种方法

---
+++ is_prime.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python
+#coding:utf-8
+
+"""
+判断一个数是否为素数的几种方法
+"""
+
+#方法一
+
+import math  
+         
+def isPrime1(n):  
+    if n <= 1:  
+        return False 
+    for i in range(2, int(math.sqrt(n)) + 1):  
+        if n % i == 0:  
+            return False
+        
+    return True
+
+#方法二
+
+def isPrime2(n):
+    if n <= 1:
+        return False
+                  
+    i = 2
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+#方法三
+
+from itertools import count
+
+def isPrime3(n):
+    if n <= 1:
+        return False
+    for i in count(2):
+        if i * i > n:
+            return True
+        if n % i == 0:
+            return Fals
+
+#方法四
+
+def isPrime4(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
+
+
+if __name__=="__main__":
+    a=isPrime4(5)
+    print a
