06caa4900c90cf2925faacd11929f35309ec2c28
qiwsir <qiwsir@gmail.com>
2014-06-19 22:45:26 +0800

    增加了collections.Counter()方法

--- same_element_in_list.md
+++ same_element_in_list.md
@@ -8,7 +8,11 @@
 
 将重复元素的列表中的重复元素进行统计，并将统计结果放在dictionary中，key为元素，value为该元素的个数
 
+**更新此步方法：**上述步骤的功能，能够通过另外一个方法实现，即collections.Counter()
+
 然后通过for获取key，得到一个新的列表，就是没有重复元素的列表
+
+
 
 #解决（Python）
 
@@ -22,6 +26,14 @@
 	        number = one_list.count(e)      #数出某个元素的个数
 	        element_number[e] = number     #生成类似：{1:1,2:3,3:5}的结果，key-element,value-元素的个数
 	    return element_number
+
+    #应用collections.Counter()实现count_element(one_list)函数功能，为了便于调试和说明，在另外一个函数里面使用
+    
+    from collections import Counter
+
+    def count_element2(one_list):
+        return Counter(one_list)
+
 	
 	def no_repeat_element(element_number):      #element_number是count_element(one_list)的返回值
 	    no_repeat_list = [key for key in element_number]
--- same_element_in_list.py
+++ same_element_in_list.py
@@ -13,6 +13,13 @@ def count_element(one_list):
         element_number[e] = number     #生成类似：{1:1,2:3,3:5}的结果，key-element,value-元素的个数
     return element_number
 
+#函数count_element(one_list)的功能，可以用collections模块中的Counter完成
+
+from collections import Counter
+
+def count_element2(one_list):
+    return Counter(one_list)
+
 def no_repeat_element(element_number):      #element_number是count_element(one_list)的返回值
     no_repeat_list = [key for key in element_number]
     return no_repeat_list
@@ -36,3 +43,21 @@ if __name__=="__main__":
     print "another way is:"
     no_repeat2 = list(set(ls))
     print no_repeat2
+    print "-------"
+    el_num2=count_element2(ls)
+    print el_num2
+    no_repeat = no_repeat_element(el_num)
+    print no_repeat
+
+"""
+the result is :
+
+{'a': 2, 'c': 2, 'b': 3}
+['a', 'c', 'b']
+another way is:
+['a', 'c', 'b']
+-------
+Counter({'b': 3, 'a': 2, 'c': 2})
+['a', 'c', 'b']
+
+"""
