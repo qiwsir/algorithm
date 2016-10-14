10b2f7a3ee9af6469d82eccf0d3067329706662b
Qiaolun Zhang <zhql0907@outlook.com>
2016-10-14 16:31:26 +0800

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -74,7 +74,7 @@ def qsort(list):
         return []
     else:
         pivot = list[0]
-        less = [x for x in list     if x <  pivot]
+        less = [x for x in list[1:] if x <  pivot]
         more = [x for x in list[1:] if x >= pivot]
         return qsort(less) + [pivot] + qsort(more)
 """
